6e7e9bbdcb9f3fb71a2368fb1e7c43e889dd466d
Parth Shah <parth2844@gmail.com>
2020-05-06 01:27:33 -0700

    Updates

---
+++ precache-manifest.9a19fb0a5e77936a35e3443938b21899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff7b7924e15ca18b08f11841e55f573",
+    "url": "/parth-shah/index.html"
+  },
+  {
+    "revision": "8bbc4d0dca5c99d7a640",
+    "url": "/parth-shah/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/parth-shah/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parth-shah/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbc4d0dca5c99d7a640",
+    "url": "/parth-shah/static/js/main.7cd9c395.chunk.js"
+  },
+  {
+    "revision": "7a952900410b77091691",
+    "url": "/parth-shah/static/js/runtime-main.39707f88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/parth-shah/static/media/logo.5d5d9eef.svg"
+  }
+]);
