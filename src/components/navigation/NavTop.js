81ab30c966a4f9a900f1ec96324cda0ec1ba034c
Parth Shah <parth2844@gmail.com>
2020-06-17 02:57:37 -0700

    Added Links to components

--- src/components/navigation/NavTop.js
+++ src/components/navigation/NavTop.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import '../../stylesheets/NavTop.css'
@@ -7,28 +8,28 @@ function NavTop() {
     return (
         
         <Navbar expand="md" variant="light" bg="light" className="border-bottom mb-3">
-            <Navbar.Brand href="/" className="logo">Parth Shah</Navbar.Brand>
+            <Navbar.Brand href="/parth-shah" className="logo">Parth Shah</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
-            <Nav variant="" defaultActiveKey="0" className="ml-auto">
+            <Nav variant="tabs" defaultActiveKey="0" className="ml-auto">
                 <Nav.Item>
-                    <Nav.Link eventKey="0" >
-                    HOME
+                    <Nav.Link eventKey="0">
+                        <Link to="/parth-shah">HOME</Link>
                     </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link eventKey="1" >
-                    SKILLS
+                        <Link to='/skills'>SKILLS</Link>
                     </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link eventKey="2" >
-                    EXPERIENCE
+                        <Link to="/experience">EXPERIENCE</Link>
                     </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link eventKey="3" >
-                    PROJECTS
+                        <Link to="/projects">PROJECTS</Link>
                     </Nav.Link>
                 </Nav.Item>
             </Nav>
