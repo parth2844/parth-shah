b6cfdf10c3f034c731993836d0a9fedc52a39c06
Parth Shah <parth2844@gmail.com>
2020-07-04 17:42:56 -0700

    Experience Page and Color Changes

--- src/components/pages/experience.js
+++ src/components/pages/experience.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import { Spring } from 'react-spring/renderprops'
+import '../../stylesheets/experience.css'
 
 function experience() {
     return (
-        
+        <div className="experience-page">
         <Spring
         from = {{ marginLeft: -500 }}
         to = {{ marginLeft:0 }}
@@ -13,8 +14,8 @@ function experience() {
         >
             {props => (
                 <div style = {props}>
-                    <Container>
-                        <h3>My Professional Work Experience...</h3>
+                    <Container className="pt-3 pb-3">
+                        <h3 className="experience-page-title">My Professional Work Experience...</h3>
                         <Spring
                         from = {{ opacity: 0 }}
                         to = {{ opacity: 1}}
@@ -22,9 +23,11 @@ function experience() {
                         >
                             {props => (
                                 <div style = {props}>
-                                    <Card bg= "light" border="dark" className="mt-4">
-                                        <Card.Header><Card.Title className="m-0">Ainves – Front End Development Intern</Card.Title></Card.Header>
-                                        <Card.Body>                                            
+                                    <Card className="experience-page-card mt-4">
+                                        <Card.Header className="experience-page-card-header">
+                                            <Card.Title className="experience-page-card-title m-0">Ainves – Front End Development Intern</Card.Title>
+                                        </Card.Header>
+                                        <Card.Body className="experience-page-card-body">                                         
                                             <Card.Text>
                                                 Involved directly in the development of company’s online e-commerce platform using React in a CI/CD environment
                                             </Card.Text>
@@ -33,9 +36,11 @@ function experience() {
                                             </Card.Text>
                                         </Card.Body>
                                     </Card>
-                                    <Card bg= "light" border="dark" className="mt-4">
-                                        <Card.Header><Card.Title className="m-0">Innovative Distributors – Web Development Intern</Card.Title></Card.Header>
-                                        <Card.Body>                                            
+                                    <Card className="experience-page-card mt-4">
+                                        <Card.Header className="experience-page-card-header">
+                                            <Card.Title className="experience-page-card-title m-0">Innovative Distributors – Web Development Intern</Card.Title>
+                                        </Card.Header>
+                                        <Card.Body className="experience-page-card-body">                                            
                                             <Card.Text>
                                                 Restructured front end of e-commerce website and integrated the database to present live inventory information
                                             </Card.Text>
@@ -53,7 +58,7 @@ function experience() {
             )}
             
         </Spring>
-        
+        </div>
     )
 }
 
