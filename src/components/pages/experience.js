d5d9be1a9bb25b45658b11d5d1487ec41a22122c
Parth Shah <parth2844@gmail.com>
2020-07-17 12:50:10 -0700

    Formatting updates

--- src/components/pages/experience.js
+++ src/components/pages/experience.js
@@ -25,28 +25,50 @@ function experience() {
                                 <div style = {props}>
                                     <Card className="experience-page-card mt-4">
                                         <Card.Header className="experience-page-card-header">
-                                            <Card.Title className="experience-page-card-title m-0">Ainves – Front End Development Intern</Card.Title>
+                                            <Card.Title className="experience-page-card-title m-0"><a href="https://www.advancedretailea.com/" target="_blank">Advance Energy Analytics</a></Card.Title>
                                         </Card.Header>
-                                        <Card.Body className="experience-page-card-body">                                         
+                                        <Card.Body className="experience-page-card-body">    
+                                            <Card.Text>  
+                                                <h5>Position : Software Engineer Intern</h5>
+                                            </Card.Text> 
                                             <Card.Text>
-                                                Involved directly in the development of company’s online e-commerce platform using React in a CI/CD environment
+                                                <h6>Role Description:</h6>
+                                                <p>Planning and assisting the deployment of company's software platform on Amazon Cloud computing environment.</p>                               
                                             </Card.Text>
-                                            <Card.Text>
-                                                Responsible for developing and maintaining webpages, email templates and performing API integrations 
-                                            </Card.Text>
+                                            <button className="details-button">Detailed Role</button>
                                         </Card.Body>
                                     </Card>
                                     <Card className="experience-page-card mt-4">
                                         <Card.Header className="experience-page-card-header">
-                                            <Card.Title className="experience-page-card-title m-0">Innovative Distributors – Web Development Intern</Card.Title>
+                                            <Card.Title className="experience-page-card-title m-0"><a href="https://www.kretiva.com/" target="_blank">Kretiva - The Collaborative Market Place</a></Card.Title>
                                         </Card.Header>
-                                        <Card.Body className="experience-page-card-body">                                            
+                                        <Card.Body className="experience-page-card-body">    
+                                            <Card.Text>  
+                                                <h5>Position : FrontEnd Developer Intern</h5>
+                                            </Card.Text> 
                                             <Card.Text>
-                                                Restructured front end of e-commerce website and integrated the database to present live inventory information
+                                                <h6>Role Description:</h6>
+                                                <p>Supported the development of company’s online e-commerce platform using React in a CI/CD environment</p>
+                                                <p>Responsible for developing and maintaining webpages, email templates and performing API integrations </p>                                  
                                             </Card.Text>
+                                            <button className="details-button">Detailed Role</button>
+                                        </Card.Body>
+                                    </Card>
+                                    <Card className="experience-page-card mt-4">
+                                        <Card.Header className="experience-page-card-header">
+                                            <Card.Title className="experience-page-card-title m-0"><a href="https://geyserwala.com/" target="_blank">Innovative Distributors – Water Heater Solutions</a></Card.Title>
+                                        </Card.Header>
+                                        <Card.Body className="experience-page-card-body">       
+                                            <Card.Text>  
+                                                <h5>Position : Web Developer Intern</h5>
+                                            </Card.Text> 
                                             <Card.Text>
-                                                Promoted and marketed products using Google AdWords platform, which increased website hits by 30-40%
+                                                <h6>Role Description:</h6>
+                                                <p>Restructured front end of e-commerce website and integrated the database to present live inventory information</p>
+                                                <p>Promoted and marketed products using Google AdWords platform, which increased website hits by 30-40%</p>                                  
                                             </Card.Text>
+                                            <button className="details-button">Detailed Role</button>                                    
+                                            
                                         </Card.Body>
                                     </Card>                                    
                                 </div>
