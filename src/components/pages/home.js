1ac6c87991a2bc8b013144529a32d3081781be68
Parth Shah <parth2844@gmail.com>
2020-07-17 13:05:51 -0700

    Home page updates

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -31,17 +31,17 @@ function home() {
                         <Tabs defaultActiveKey="about" variant="pills">
                             <Tab eventKey="about" title="About" className="pt-3 tab-details">
                                 <h3><i>Something About Me...</i></h3>
-                                <p>I am a Master of Computer Science student at University of California, Irvine graduating in December 2020.</p>
-                                <p>Currently, I am working as a FrontEnd Developer Intern at Kretiva.</p>
-                                <p>I am also working on personal side projects to enhance my skills in React, Node and MongoDB.</p>
+                                <p>I am a student at University of California, Irvine. Originally I’m from Mumbai, India and moved to USA to pursue my Master’s Degree in Computer Science. I also hold a Bachelor of Computer Engineering Degree from Mumbai University.</p>
+                                <p>As the world today builds itself around technology, it is difficult to find any application which remains stranger to Artificial Intelligence or Machine Learning. One of these applications, which sparked my interest in Computer Science came from my favourite sport, Cricket. A lightweight sticker that could be attached to cricket bats, to collect real-time data on several parameters of a cricket shot was in development. Even apart from this very interesting titbit, there were so many amazing innovations coming around in technology, I had to be a part of it.</p>
+                                <p>Apart from this, I enjoy watching television sitcoms, movies and documentaries in my leisure time. Recently, I’ve also started to read novels and biographies in pursuit of developing a new hobby.</p>
                                 <p>Connect with me on LinkedIn using the link below to know more.</p>
                             </Tab>
                             <Tab eventKey="summary" title="Summary" className="pt-3 tab-details">
-                                <h3><i>Short summary of my skills...</i></h3>                                
-                                <p>Strong analytical skills in Algorithms, Data Structures and Problem Solving</p>
-                                <p>Experienced in development of web-based applications in JavaScript, Java and Python</p>
-                                <p>Quick Learner, eager to develop new skills in Software Development</p>
-                                <p>Disciplined and consistent performer in team projects</p>
+                                <h3><i>Ongoing Activities...</i></h3>                                
+                                <p>Alongside my Master’s Degree, I’m also engaged in part-time internships and projects to develop my expertise and skills set. </p>
+                                <p>Currently, I’m working as a Front-End Developer for Kretiva and as a Software Developer for Advanced Energy Analytics. Working at these companies has helped me develop some amazing skills, even apart from technology. Look at my Experience page for a more detailed description of my journey.</p>
+                                <p>Apart from internships, I’m also working to enhance my skills through personal projects during my spare time in summer. For details about my personal projects, please visit the Projects page.</p>
+                                
                             </Tab>                            
                         </Tabs>                        
                     </Container>
